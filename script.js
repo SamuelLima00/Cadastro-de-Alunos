43d2ce1d8ed9f220960a8cf2594be36f28fef384
Juliana Souza <lj.m.msouza@gmail.com>
2026-06-17 22:45:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,105 @@
-llet alunos =
+let alunos = 
 JSON.parse(localStorage.getItem("alunos")) || [];
 
 function mostrarTela(id){
+    if(
+        (id === "cadastro" || id === "lista") &&
+        localStorage.getItem("logado") !== "true"
+    ){
+
+        alert("Faça login primeiro!");
+        id = "login";
+
+    }
 
     document
     .querySelectorAll("section")
-    .forEach(sec =>
-        sec.classList.add("oculto")
-    );
+    .forEach(sec => {
+        sec.classList.add("oculto");
+
+    });
 
     document
     .getElementById(id)
     .classList.remove("oculto");
+
 }
 
-function salvarAluno(){
+function cadastrarUsuario(){
+    const usuario =
+    document.getElementById("novoUsuario").value;
+    const senha =
+    document.getElementById("novaSenha").value;
 
-    const nome =
-    document.getElementById("nome").value;
-
-    const matricula =
-    document.getElementById("matricula").value;
-
-    if(nome === "" || matricula === ""){
-        alert("Preencha os campos obrigatórios");
+    if(usuario === "" || senha === ""){
+        alert("Preencha todos os campos");
         return;
     }
 
-    const existe =
-    alunos.find(
+    localStorage.setItem(
+        "usuario",
+        JSON.stringify({
+            usuario: usuario,
+            senha: senha
+
+        })
+
+    );
+    alert("Usuário criado com sucesso!");
+    mostrarTela("login");
+}
+
+function fazerLogin(){
+    const usuario =
+    document.getElementById("usuario").value;
+    const senha =
+    document.getElementById("senha").value;
+    const dados =
+    JSON.parse(
+        localStorage.getItem("usuario")
+
+    );
+    if(!dados){
+        alert("Crie uma conta primeiro!");
+        mostrarTela("cadastroUsuario");
+        return;
+    }
+
+    if(
+
+        usuario === dados.usuario &&
+        senha === dados.senha
+
+    ){
+        localStorage.setItem(
+            "logado",
+            "true"
+        );
+
+        alert("Login realizado!");
+        mostrarTela("home");
+    }else{
+        alert("Usuário ou senha incorretos");
+    }
+}
+
+function logout(){
+    localStorage.removeItem("logado");
+    alert("Logout realizado!");
+    mostrarTela("login");
+
+}
+
+function salvarAluno(){
+    const nome =
+    document.getElementById("nome").value;
+    const matricula =
+    document.getElementById("matricula").value;
+    if(nome === "" || matricula === ""){
+        alert("Preencha nome e matrícula");
+        return;
+    }
+    const existe = alunos.find(
         aluno => aluno.nome === nome
     );
 
@@ -37,24 +108,114 @@ function salvarAluno(){
         return;
     }
 
-   alunos.push({
+    alunos.push({
 
-    nome,
-    matricula,
+        nome,
+        matricula,
+        idade:
+        document.getElementById("idade").value,
+        curso:
+        document.getElementById("curso").value,
+        escola:
+        document.getElementById("escola").value,
+        foto:
+        document.getElementById("fotoPerfil").src
 
-    idade:
-    document.getElementById("idade").value,
+    });
 
-    curso:
-    document.getElementById("curso").value,
+    localStorage.setItem(
+        "alunos",
+        JSON.stringify(alunos)
 
-    escola:
-    document.getElementById("escola").value,
+    );
 
-    foto:
-    document.getElementById("fotoPerfil").src
+    listarAlunos();
+    limparFormulario();
+    mostrarTela("lista");
 
-});
+}
+
+function listarAlunos(){
+
+    const busca =
+    document.getElementById("busca")?.value
+    .toLowerCase() || "";
+    const tabela =
+    document.getElementById("tabela");
+    if(!tabela) return;
+    tabela.innerHTML = "";
+    alunos
+    .filter(aluno =>
+
+        aluno.nome.toLowerCase().includes(busca)
+        ||
+        aluno.matricula.toLowerCase().includes(busca)
+
+    )
+
+    .forEach((aluno,index)=>{
+        tabela.innerHTML += `
+
+        <tr>
+        <td>
+        <img
+
+        src="${aluno.foto}"
+        width="50"
+        height="50"
+        style="border-radius:50%">
+
+        </td>
+        <td>${aluno.nome}</td>
+        <td>${aluno.matricula}</td>
+        <td>${aluno.idade || "N/A"}</td>
+        <td>${aluno.curso || "N/A"}</td>
+        <td>${aluno.escola || "N/A"}</td>
+        <td>
+        <button onclick="editarAluno(${index})">
+        ✏️
+        </button>
+        <button onclick="excluirAluno(${index})">
+        🗑️
+        </button>
+        </td>
+        </tr>
+        `;
+    });
+}
+
+function editarAluno(index){
+
+    const aluno = alunos[index];
+    aluno.nome = prompt(
+        "Nome:",
+        aluno.nome
+
+    );
+
+    aluno.matricula = prompt(
+        "Matrícula:",
+        aluno.matricula
+
+    );
+
+    aluno.idade = prompt(
+        "Idade:",
+        aluno.idade
+
+    );
+
+    aluno.curso = prompt(
+        "Curso:",
+        aluno.curso
+
+    );
+
+    aluno.escola = prompt(
+        "Escola:",
+        aluno.escola
+
+    );
 
     localStorage.setItem(
         "alunos",
@@ -63,65 +224,6 @@ function salvarAluno(){
 
     listarAlunos();
 
-    mostrarTela("lista");
-    document.getElementById("nome").value = "";
-    document.getElementById("matricula").value = "";
-    document.getElementById("idade").value = "";
-    document.getElementById("curso").value = "";
-    document.getElementById("escola").value = "";
-    document.getElementById("fotoPerfil").src = "img/perfil-padrao.png";
-}
-
-function listarAlunos(){
-
-    const busca =
-    document
-    .getElementById("busca")
-    ?.value
-    .toLowerCase() || "";
-
-    const tabela =
-    document.getElementById("tabela");
-
-    tabela.innerHTML = "";
-
-    alunos
-    .filter(a =>
-    a.nome.toLowerCase().includes(busca) ||
-    a.matricula.toLowerCase().includes(busca)
-    )
-    .forEach((aluno,index)=>{
-
-        tabela.innerHTML += `
-        <tr>
-            <td>
-            <img src="${aluno.foto}"
-            width="50"
-            height="50"
-            style="border-radius:50%;">
-            </td>
-            <td>${aluno.nome}</td>
-            <td>${aluno.matricula}</td>
-            <td>${aluno.idade || "N/A"}</td>
-            <td>${aluno.curso || "N/A"}</td>
-            <td>${aluno.escola || "N/A"}</td>
-
-            <td>
-
-                <button
-                onclick="editarAluno(${index})">
-                ✏️
-                </button>
-
-                <button
-                onclick="excluirAluno(${index})">
-                🗑️
-                </button>
-
-            </td>
-        </tr>
-        `;
-    });
 }
 
 function excluirAluno(index){
@@ -136,56 +238,45 @@ function excluirAluno(index){
         );
 
         listarAlunos();
+
     }
 }
 
-function editarAluno(index){
+function limparFormulario(){
 
-    const novoNome = prompt(
-    "Novo nome:",
-    alunos[index].nome
-);
+document.getElementById("nome").value="";
+document.getElementById("matricula").value="";
+document.getElementById("idade").value="";
+document.getElementById("curso").value="";
+document.getElementById("escola").value="";
+document.getElementById("fotoPerfil").src=
 
-if(
-    alunos.some(
-        (a,i) =>
-        i !== index &&
-        a.nome === novoNome
-    )
-){
-    alert("Já existe um aluno com esse nome.");
-    return;
+"img/perfil-padrao.png";
 }
 
-    if(novoNome){
+const uploadFoto =
+document.getElementById("uploadFoto");
+const fotoPerfil =
+document.getElementById("fotoPerfil");
 
-        alunos[index].nome = novoNome;
+    if(uploadFoto){
+        uploadFoto.addEventListener(
+        "change",
 
-        localStorage.setItem(
-            "alunos",
-            JSON.stringify(alunos)
-        );
+function(){
 
-        listarAlunos();
-    }
-}
-
-listarAlunos();
-
-const uploadFoto = document.getElementById("uploadFoto");
-const fotoPerfil = document.getElementById("fotoPerfil");
-
-uploadFoto.addEventListener("change", function() {
     const arquivo = this.files[0];
-
-    if (arquivo) {
+    if(arquivo){
         const leitor = new FileReader();
+        leitor.onload = function(e){
+        fotoPerfil.src = e.target.result;
 
-        leitor.onload = function(e) {
-            fotoPerfil.src = e.target.result;
-        };
+}
 
-        leitor.readAsDataURL(arquivo);
-    }
-});
+leitor.readAsDataURL(arquivo);
 
+}
+}
+);
+}
+listarAlunos();
